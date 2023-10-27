66881845d7235d01661133a95ba5c2760515e4cb
gabrishow <gabriel_neves1@outlook.com>
2023-10-26 21:44:44 -0300

    Desafio finalizado

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log("olá marilene");
+let nome = "Gabriel"
+let xp = 0
+
+if (xp < 1000)
+{
+    console.log("O " + nome + " é Ferro ");
+}
+else if (xp < 2001)
+{
+    console.log("O " + nome + " é Bronze ");
+}
+else if (xp < 6001){
+
+    console.log("O " + nome + " é Prata ");
+    }
+else if (xp < 7001)
+{
+    console.log("O " + nome + " é Ouro ");
+}
+else if (xp < 8001)
+{
+    console.log("O " + nome + " é Platina ");
+}
+else if (xp < 9001)
+{
+    console.log("O " + nome + " é Ascendente ");
+}
+else
+{
+    console.log("O " + nome + " é Radiante ");
+}
